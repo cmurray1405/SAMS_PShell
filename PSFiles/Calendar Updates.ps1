640d6c2d6fe6a8c70e37361caf68ae51eddfe1b2
cmurray1405 <51841043+cmurray1405@users.noreply.github.com>
2020-02-10 16:32:52 +1100

    Update Calendar Updates.ps1

--- PSFiles/Calendar Updates.ps1
+++ PSFiles/Calendar Updates.ps1
@@ -1,7 +1,2 @@
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User belinda.waldock@samaritans.org.au -AccessRights Reviewer
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User kathy.alley@samaritans.org.au -AccessRights Editor
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User lakshmi.rajendran@samaritans.org.au -AccessRights Reviewer
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User patricia.temple@samaritans.org.au -AccessRights Reviewer
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User priscilla.duraisamy@samaritans.org.au -AccessRights Reviewer
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User tracy.brack@samaritans.org.au -AccessRights Reviewer
-add-MailboxFolderPermission -Identity mnceceicar:\calendar -User yuka.green@samaritans.org.au -AccessRights Reviewer
+$user ="string"
+import-csv -path C:\Temp\Calendar.csv | ForEach-Object {add-MailboxFolderPermission -Identity $user -User $_.upn -AccessRights }
