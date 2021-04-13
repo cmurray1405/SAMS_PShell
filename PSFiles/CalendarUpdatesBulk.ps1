18be6fa9cb0169a50d031f33fd499cd414f270c0
cmurray1405 <craig.murray@samaritans.org.au>
2021-04-13 15:43:20 +1000

    Update Bulk script
    Update bulk script to include login and variable

--- PSFiles/CalendarUpdatesBulk.ps1
+++ PSFiles/CalendarUpdatesBulk.ps1
@@ -1,8 +1,16 @@
-#Prompt for user
-$calendar = Read-Host -Prompt "What is the name of the users calendar?"
+#Admin Login
+$admin = Read-Host -Prompt "Enter Admin Email"
+
+Connect-ExchangeOnline -UserPrincipalName $admin
+
+#Prompt for Calendar
+$calendar = Read-Host -Prompt "What is alias for the calendar?"
+
+#Access Rights
+$access = Read-Host -Prompt "what access rights are needed"
 
 #Loop though multiple users based on CSV
-import-csv -path C:\Temp\Calendar.csv | ForEach-Object {add-MailboxFolderPermission -Identity ${calendar}:\calendar -User $_.upn -AccessRights }
+import-csv -path C:\Users\craig.murray\Github\SAMS_PShell\CSV\Calendar.csv | ForEach-Object {add-MailboxFolderPermission -Identity ${calendar}:\calendar -User $_.upn -AccessRights }
 
 #Confirm user is added
 Get-MailboxFolderPermission -Identity ${calendar}:\calendar
